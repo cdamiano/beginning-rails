ae0bf939fd21507ca48cefdb307047ef761f125d
con.damianopoulos <con.damianopoulos@intunity.com.au>
2013-11-12 12:59:51 +1100

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '3f1c2fd521fea2ef9411e51fbc3bd198d80138bd4b33b3093a638596203ad7ff5acc6363476bc816e4c027f72acf34c35aaf0e00362cac3dfca389bdccf55a69'
